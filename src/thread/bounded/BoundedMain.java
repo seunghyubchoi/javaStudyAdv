5086d7eeed279335c66793d466cd0ea1d1665cd6
Seunghyub Choi <seunghyub.choi@gmail.com>
2024-10-08 20:29:34 +0900

    producer consumer2

--- src/thread/bounded/BoundedMain.java
+++ src/thread/bounded/BoundedMain.java
@@ -9,7 +9,8 @@ import static util.ThreadUtils.sleep;
 public class BoundedMain {
     public static void main(String[] args) {
         // 1. BoundedQueue 선택
-        BoundedQueue queue = new BoundedQueueV1(2);
+        //BoundedQueue queue = new BoundedQueueV1(2);
+        BoundedQueue queue = new BoundedQueueV2(2);
 
         // 2. 생산자, 소비자 실행 순서 선택, 주석으로 반드시 하나만 선택
         producerFirst(queue); // 생성자 먼저 실행
