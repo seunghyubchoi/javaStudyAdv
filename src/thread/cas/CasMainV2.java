9ba21b627b6ed025e5f5ffa5d2a0900b89dc8f6e
Seunghyub Choi <seunghyub.choi@gmail.com>
2024-10-09 14:17:07 +0900

    spinLock

---
+++ src/thread/cas/CasMainV2.java
@@ -0,0 +1,31 @@
+package thread.cas;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+import static util.MyLogger.log;
+
+public class CasMainV2 {
+    public static void main(String[] args) {
+        AtomicInteger atomicInteger = new AtomicInteger(0);
+        System.out.println("start value = " + atomicInteger.get());
+
+        int result1 = incrementAndGet(atomicInteger);
+        System.out.println("result1 : " + result1);
+
+        int result2 = incrementAndGet(atomicInteger);
+        System.out.println("result2 : " + result2);
+    }
+
+    // increamentAndGet 구현
+    private static int incrementAndGet(AtomicInteger atomicInteger){
+        int getValue;
+        boolean result;
+        do {
+            getValue = atomicInteger.get();
+            log("getValue = " + getValue);
+            result = atomicInteger.compareAndSet(getValue, getValue+1);
+            log("result : " + result);
+        } while (!result);
+        return getValue + 1;
+    }
+}
---
+++ src/thread/cas/spinlock/SpinLockMain.java
@@ -0,0 +1,29 @@
+package thread.cas.spinlock;
+
+import static util.MyLogger.log;
+
+public class SpinLockMain {
+    public static void main(String[] args) {
+        //SpinLockBad spinLock = new SpinLockBad();
+        SpinLock spinLock = new SpinLock();
+
+        Runnable runnable = new Runnable() {
+
+            @Override
+            public void run() {
+                spinLock.lock();
+                try {
+                    log("비지니스 로직 실행");
+
+                } finally {
+                    spinLock.unlock();
+                }
+            }
+        };
+
+        Thread t1 = new Thread(runnable, "Thread-1");
+        Thread t2 = new Thread(runnable, "Thread-2");
+        t1.start();
+        t2.start();
+    }
+}
